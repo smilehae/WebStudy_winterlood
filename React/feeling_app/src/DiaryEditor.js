e5fe50792c53081758dfa63fdc461ea72523d82c
smilehae <mihaepyun@gmail.com>
2022-01-07 03:08:14 +0900

    기본 일기장 만들기 시작

---
+++ React/feeling_app/src/DiaryEditor.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  // const [author, setAuthor] = useState("");
+  // const [content, setContext] = useState("");
+
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장성공");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          name="content"
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
